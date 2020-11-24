3f4f97b492bbbd77f2005484884109a80b18cae0
@HarveyThePooka404 <sylv1612@stud.kea.dk>
2020-11-24 13:38:03 +0100

    added the css-grid for components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import { _GET } from "./db/Db";
 import QueueTime from "./components/QueueTime";
+import bar from "./assets/bar.jpg";
 
 import "./App.css";
 
@@ -20,13 +21,48 @@ function App() {
   }, []);
 
   return (
+
     <div className="App">
-      <QueueTime queue={data.queue ? data.queue.length : "0"}></QueueTime>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="header">
+        <img src={bar} alt="header picture" />
+      </div>
+      <div className="sidebar">this is a sidebar with content</div>
+
+      {/* number of people in queue */}
+      <section>
+        <QueueTime queue={data.queue ? data.queue.length : "0"}></QueueTime>
+      </section>
+
+      {/* time to bar closes */}
+      <section>
+        <QueueTime queue={data.queue ? data.queue.length : "0"}></QueueTime>
+      </section>
+
+      {/* customers being served */}
+      <section>
+        <QueueTime queue={data.queue ? data.queue.length : "0"}></QueueTime>
+      </section>
+
+      {/* bartenders */}
+      <section>
+        <QueueTime queue={data.queue ? data.queue.length : "0"}></QueueTime>
+      </section>
+
+      {/* taps */}
+      <section>
+        <QueueTime queue={data.queue ? data.queue.length : "0"}></QueueTime>
+      </section>
+
+      {/* storage */}
+      <section>
+        <QueueTime queue={data.queue ? data.queue.length : "0"}></QueueTime>
+      </section>
+
+      <div className="footer"> this is a footer with some information</div>
     </div>
+
+
+
   );
 }
 
